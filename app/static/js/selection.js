cf845c9e4fb7b459e67753f47c7d2001da1c1c9d
Hugo291 <33847075+Hugo291@users.noreply.github.com>
2018-04-22 23:55:46 +0200

    update first page load on start

--- app/static/js/selection.js
+++ app/static/js/selection.js
@@ -1,79 +1,12 @@
 $(function () {
 
-    var last_boxs = null;
-
     $('.page-element-selection').click(function() {
-
-
-
-        $('.page-element-selection').removeClass("select");
-        $(this).addClass('select');
-
-        console.log("select");
-
-        //pdf_id and file is the page
-        var pdf_id = $(this).data('pdf_id');
-        var page_number = $(this).data('page_number');
-
-        startLoad("waiting for the page "+(page_number+1));
-
-        console.log('pdf-id : '+pdf_id+" page_number : "+page_number);
-
-        //the image
-        var src = $(this).find("img").attr('src');
-
-        //canvas
-        var canvas = document.getElementById("canvas-selection"),
-        ctx = canvas.getContext("2d");
-
-        //image background convas
-        var background = new Image();
-        background.src = src;
-
-        //set canvas size
-        canvas.width = background.width;
-        canvas.height = background.height;
-
-        //set size of
-        $('#textarea-text-page').height = background.height;
-        $('.scroll-list-image').height = background.height;
-
-
-        canvas.background= src;
-
-        background.onload = function(){
-            ctx.drawImage(background,0,0);
-        };
-
-        //get info of scan page box(word) and text
-        $.ajax({url: "/scan/page/"+pdf_id+"/"+page_number, success: function(results){
-
-            //boxs word
-            var boxs = results.box;
-            last_boxs = boxs;
-            //create all box
-            for(let index in boxs){
-                var box = boxs[index];
-                ctx.rect(box.position_left,box.position_top,box.size_width,box.size_height);
-                ctx.stroke();
-                ctx.lineWidth=1;
-                ctx.strokeStyle="#FF0000";
-            }
-
-            //text page
-            var text = results.text;
-
-            $('#textarea-text-page').val(text);
-
-            endLoad('The page is loaded');
-        }});
-
+        searchBox(this);
     });
 
+    searchBox($('.page-element-selection:first'));
 
 });
-
-
 
 $(function () {
     $('.btn-correction').click(function () {
@@ -100,6 +33,10 @@ function startLoad(text) {
     );
 }
 
+function setBackgroundSelect(CurrentSelect) {
+    $('.page-element-selection').removeClass("bg-secondary");
+    $(CurrentSelect).addClass('bg-secondary');
+}
 
 function endLoad(text) {
     $('#load-process').html("" +
@@ -109,3 +46,72 @@ function endLoad(text) {
             "</div>" +
         "</div>")
 }
+
+function setInfoPage(ctx , pdf_id , page_number) {
+     //get info of scan page box(word) and text
+        $.ajax({url: "/scan/page/"+pdf_id+"/"+page_number, success: function(results){
+
+            //boxs word
+            var boxs = results.box;
+            last_boxs = boxs;
+            //create all box
+            for(let index in boxs){
+                let box = boxs[index];
+                ctx.rect(box.position_left,box.position_top,box.size_width,box.size_height);
+                ctx.stroke();
+                ctx.lineWidth=1;
+                ctx.strokeStyle="#FF0000";
+            }
+
+            setTextAreaText(results.text);
+
+            endLoad('The page is loaded');
+        }});
+}
+
+function setTextAreaText(text) {
+    $('#textarea-text-page').val(text);
+}
+
+function canvasBox(currentSelect) {
+    var src = $(currentSelect).find("img").attr('src');
+
+    //canvas
+    var canvas = document.getElementById("canvas-selection"),
+    ctx = canvas.getContext("2d");
+
+    //image background convas
+    var background = new Image();
+    background.src = src;
+
+    //set canvas size
+    canvas.width = background.width;
+    canvas.height = background.height;
+
+    //set size of
+    $('#textarea-text-page').height = background.height;
+    $('.scroll-list-image').height = background.height;
+
+    canvas.background= src;
+
+    background.onload = function(){
+        ctx.drawImage(background,0,0);
+    };
+
+    return ctx;
+}
+
+function searchBox(currentSelect) {
+
+    setBackgroundSelect(currentSelect);
+
+    var pdf_id = $(currentSelect).data('pdf_id');
+    var page_number = $(currentSelect).data('page_number');
+
+    startLoad("waiting for the page "+(page_number+1));
+
+    let ctx = canvasBox(currentSelect);
+
+    setInfoPage(ctx , pdf_id , page_number);
+}
+
